1fedaa00fae122526b6bfb153cc52efd469269eb
NattapatDaengkongkaew <nattapatque.daengkongkaew@gmail.com>
2025-12-27 22:18:16 +0700

    experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/sectionComponents/heroSection";
 import SkillSection from "../components/sectionComponents/skillSection";
+import ExperienceSection from "../components/sectionComponents/experienceSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection></HeroSection>
       <SkillSection></SkillSection>
+      <ExperienceSection></ExperienceSection>
     </div>
   );
 }
